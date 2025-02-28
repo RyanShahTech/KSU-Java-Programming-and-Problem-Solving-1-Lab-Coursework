e612e04ae49ca27f865d4abee8f6a413d922bf10
Ryan Shah <166070283+RyanShahTech@users.noreply.github.com>
2025-02-28 15:32:52 -0500

    Add files via upload

---
+++ Lab2B.java
@@ -0,0 +1,15 @@
+/*
+Class: CSE 1321L
+Section: J63
+Term: Spring 2021
+Instructor: Deeksha Kaya
+Name: Ryan Shah
+Lab#: Lab 2B
+*/
+
+public class Lab2B
+{
+    public static void main (String[] args) {
+        System.out.println(" ___*___\n __*_*__ \n _*_*_*_ \n *_*_*_* \n _*_*_*_ \n __*_*__ \n ___*___ " );
+    }
+}
